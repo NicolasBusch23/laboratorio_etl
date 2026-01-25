22e06a39d5a50497c54e5887c10ddf025c5966a3
Nicolás <nicolasbusch5@gmail.com>
2026-01-24 19:35:14 -0500

    Se modifica 'main.py' de tal manera que se pueda crear la App de manera apropiado usando FastAPI. Así mismo, se incluyen algunos comentarios para mejorar el entendimiento del código

--- app/main.py
+++ app/main.py
@@ -1,25 +1,25 @@
 from .database import engine, Base
-from .controllers import persona_controller
+from .controllers import etl_controller
 from .error_handlers import register_exception_handlers
 from fastapi import FastAPI
 
 def create_app() -> FastAPI:
     """Application factory."""
-    app = FastAPI(title="FastAPI Persona CRUD (MySQL)", version="1.0.0")
+    app = FastAPI(title="ETL FreeToGame API", version="1.0.0")
 
     # Register global exception handlers (domain → HTTP)
     register_exception_handlers(app)
 
-    @app.on_event("startup")
+    @app.on_event("startup") #Crea automáticamente las tablas definidas en models en caso de que no existan
     def on_startup() -> None:
-        # Create tables at startup (demo purpose)
         Base.metadata.create_all(bind=engine)
 
-    @app.get("/health")
+    @app.get("/health") #Permite probar que la app corra correctamente.
     def health() -> dict:
         return {"status": "ok"}
 
-    app.include_router(persona_controller.router)
+    app.include_router(etl_controller.router) #Conecta a la aplicación principal los endpoint definidos en el módulo etl_controller
+
     return app
 
 
