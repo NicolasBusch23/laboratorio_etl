2cc17b78e61d047c8cacc5ac4de4d85d772c61ec
Nicolás <nicolasbusch5@gmail.com>
2026-01-25 04:53:50 -0500

    Se añade una descripción del endpoint EXTRACT  de tal manera que se pueda entender su función al consultar /docs

--- app/controllers/etl_controller.py
+++ app/controllers/etl_controller.py
@@ -2,12 +2,19 @@ from fastapi import APIRouter, status
 from app.services.etl_service import extract_games_to_mongo
 from app.views.schemas import ExtractRequest
 
-router = APIRouter(prefix="/etl_juegos", tags=["ETL Juegos"])
+router = APIRouter(prefix="/etl", tags=["ETL Juegos"])
 
-@router.post("/extract", status_code=status.HTTP_201_CREATED)
+@router.post("/extraer", status_code=status.HTTP_201_CREATED)
 def extract(request: ExtractRequest):
     """
-    Paso EXTRACT: API -> MongoDB (RAW)
+    Ejecuta el paso EXTRACT del proceso ETL.
+
+    - Consume la API pública de juegos.
+    - Extrae una cantidad definida de registros.
+    - Almacena los datos crudos (RAW) en MongoDB.
+    - Garantiza idempotencia usando el ID del juego como clave primaria.
+
+    Retorna un mensaje de cuántos registros fueron guardados y si fue exitosa la solicitud.
     """
     result = extract_games_to_mongo(request.cantidad)
     return result
