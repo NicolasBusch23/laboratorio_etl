e02b148c2380f7263acc3425f4af3dea774585f7
Pablo <juansean527@gmail.com>
2026-02-02 21:59:31 -0500

    se agrega un type ignore en el archivo error_handlers.py

--- app/services/error_handlers.py
+++ app/services/error_handlers.py
@@ -3,4 +3,4 @@ from app.services.errors import ETLError, etl_error_handler
 
 def register_exception_handlers(app: FastAPI) -> None:
     """Registra los manejadores de excepciones globales en la aplicación FastAPI."""
-    app.add_exception_handler(ETLError, etl_error_handler)
+    app.add_exception_handler(ETLError, etl_error_handler) #type: ignore
